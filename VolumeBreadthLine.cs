5faf925bcb11cb42ba3a0fc249e5d6d4110daee5
mtharmer <michaeltharmer@gmail.com>
2023-08-04 15:31:25 -0400

    Finalize issues with tickers

--- VolumeBreadthLine.cs
+++ VolumeBreadthLine.cs
@@ -20,12 +20,12 @@ namespace VolumeBreadthLine
         public VolumeBreadthLine() : base()
         {
             this.Name = "VolumeBreadthLine";
-            this.Description = "My indicator's annotation";
+            this.Description = "Displays ratios of NYSE and NASDAQ volume spreads";
             this.SeparateWindow = true;
-            this.AddLineSeries("Green Ratio", Color.Lime, 1, LineStyle.Histogramm);
-            this.AddLineSeries("Red Ratio", Color.Red, 1, LineStyle.Histogramm);
-            this.AddLineSeries("Green Ratio", Color.Green, 1, LineStyle.Histogramm);
-            this.AddLineSeries("Red Ratio", Color.Orange, 1, LineStyle.Histogramm);
+            this.AddLineSeries("NYSE Positive", Color.Lime, 4, LineStyle.Histogramm);
+            this.AddLineSeries("NYSE Negative", Color.Red, 4, LineStyle.Histogramm);
+            this.AddLineSeries("NASDAQ Positive", Color.CadetBlue, 2, LineStyle.Histogramm);
+            this.AddLineSeries("NASDAQ Negative", Color.Orange, 2, LineStyle.Histogramm);
             this.AddLineLevel(2.0, "Upper Strength", Color.Lime, 1, LineStyle.Dash);
             this.AddLineLevel(0.0, "Midline", Color.WhiteSmoke, 1, LineStyle.Dash);
             this.AddLineLevel(-5.0, "Lower Strength", Color.Red, 1, LineStyle.Dash);
@@ -33,26 +33,29 @@ namespace VolumeBreadthLine
 
         protected override void OnInit()
         {
-            int actualDays = this.histDays * (-1);
+            int actualDays = Math.Abs(this.histDays) * (-1);
             this.uvolData = Core.Instance.Symbols.FirstOrDefault(s => s.Name == "$UVOL" && s.Connection.Name == "dxFeed").GetHistory(Period.MIN15, Core.TimeUtils.DateTimeUtcNow.AddDays(actualDays));
             this.dvolData = Core.Instance.Symbols.FirstOrDefault(s => s.Name == "$DVOL" && s.Connection.Name == "dxFeed").GetHistory(Period.MIN15, Core.TimeUtils.DateTimeUtcNow.AddDays(actualDays));
             this.uvolqData = Core.Instance.Symbols.FirstOrDefault(s => s.Name == "$UVOL/Q" && s.Connection.Name == "dxFeed").GetHistory(Period.MIN15, Core.TimeUtils.DateTimeUtcNow.AddDays(actualDays));
             this.dvolqData = Core.Instance.Symbols.FirstOrDefault(s => s.Name == "$DVOL/Q" && s.Connection.Name == "dxFeed").GetHistory(Period.MIN15, Core.TimeUtils.DateTimeUtcNow.AddDays(actualDays));
-
         }
 
         protected override void OnUpdate(UpdateArgs args)
         {
-
             var time = this.Time();
             int offset = (int)this.uvolData.GetIndexByTime(time.Ticks);
+            int qoffset = (int)this.uvolqData.GetIndexByTime(time.Ticks);
 
-            if (offset < 0)
+            if (offset < 0 || qoffset < 0)
                 return;
 
             double bv = GetClose(this.uvolData, offset);
             double sv = GetClose(this.dvolData, offset);
             double ratio = (bv > sv) ? bv / sv : (sv / bv) * (-1.0);
+
+            double qbv = GetClose(this.uvolqData, qoffset);
+            double qsv = GetClose(this.dvolqData, qoffset);
+            double qratio = (qbv > qsv) ? qbv / qsv : (qsv / qbv) * (-1.0);
 
             if (ratio > 0)
             {
@@ -65,24 +68,15 @@ namespace VolumeBreadthLine
                 this.SetValue(ratio, 1);
             }
 
-            offset = (int)this.uvolqData.GetIndexByTime(time.Ticks);
-
-            if (offset < 0)
-                return;
-
-            bv = GetClose(this.uvolqData, offset);
-            sv = GetClose(this.dvolqData, offset);
-            ratio = (bv > sv) ? bv / sv : (sv / bv) * (-1.0);
-
-            if (ratio > 0)
+            if (qratio > 0)
             {
-                this.SetValue(ratio, 2);
+                this.SetValue(qratio, 2);
                 this.SetValue(0, 3);
             }
             else
             {
                 this.SetValue(0, 2);
-                this.SetValue(ratio, 3);
+                this.SetValue(qratio, 3);
             }
         }
 
@@ -124,7 +118,7 @@ namespace VolumeBreadthLine
             double numq = this.GetValue(0, 2);
             if (numq == 0.0)
                 numq = this.GetValue(0, 3);
-
+            
             graphics.DrawString("NYSE:   " + this.FormatPrice(num), font, num > 0.0 ? Brushes.Green : Brushes.Red, 50, 50);
             graphics.DrawString("NASDAQ: " + this.FormatPrice(numq), font, numq > 0.0 ? Brushes.Green : Brushes.Red, 50, 75);
         }
